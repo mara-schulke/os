2c0ef2daac02527d75d3444d4ad1843816273771
Mara Schulke <mara.schulke@helsing.ai>
2025-02-27 00:08:15 +0100

    Hints

---
+++ home/editor/nvim/editing/hints.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  programs.nixvim = {
+    keymaps = [
+      #{
+      #  key = "<leader>i";
+      #  action = ":ToggleRustHints<CR>";
+      #  options.desc = "Rust Inlay Hints";
+      #}
+    ];
+  };
+}
