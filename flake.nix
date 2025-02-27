b0fd9031831e5ebc16469a5266c0f329693c2e9e
Mara Schulke <mara.schulke@helsing.ai>
2025-02-27 15:09:50 +0100

    Nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mara's Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
