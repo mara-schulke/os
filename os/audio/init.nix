fd2b31de519bed4174b9ea5cd858cffdc5e99fb4
Mara Schulke <mail@maras.cloud>
2025-04-09 09:06:06 +0200

    Rework audio configuration

--- os/audio/init.nix
+++ os/audio/init.nix
@@ -1,31 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  sound.enable = true;
-  nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio = {
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    daemon = {
-      config = {
-        enable-deferred-volume = "no";
-      };
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #jack.enable = true;
+  };
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
     };
-    package = pkgs.pulseaudioFull; 
-    #extraModules = [ pkgs.pulseaudio-modules-bt ];
-    extraConfig = ''
-      load-module module-bluetooth-policy
-      load-module module-bluetooth-discover
-      load-module module-switch-on-connect
-    '';
   };
-
-hardware.bluetooth.settings = {
-  General = {
-    Enable = "Source,Sink,Media,Socket";
-  };
-};
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
-  hardware.bluetooth.hsphfpd.enable = true;
-  hardware.bluetooth.package = pkgs.bluezFull;
 }
