81ee7d00fe37ff3e1b021b11dc77b4b6ecade608
Mara Schulke <mail@maras.cloud>
2025-06-22 18:25:46 +0200

    Downgrade for bluetooth

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mara's Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
