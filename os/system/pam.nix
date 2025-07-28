7a162c2a46696db7d9b7d8c5c83f31504dfb9d90
Mara Schulke <mail@maras.cloud>
2025-07-28 19:40:15 +0200

    Add yubikey / pam

--- os/system/pam.nix
+++ os/system/pam.nix
@@ -1,7 +1,12 @@
-{ config, ... }:
+{ ... }:
 
 {
   # services.fprintd.enable = true;
   # security.pam.services.login.fprintAuth = true;
   # security.pam.services.xscreensaver.fprintAuth = true;
+
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
 }
