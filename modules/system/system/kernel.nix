d4dd614d66a79c4eb2d5895c661e128d8aba5107
Mara Schulke <mail@maras.cloud>
2025-07-29 23:30:42 +0200

    Remove v4l2

--- modules/system/system/kernel.nix
+++ modules/system/system/kernel.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   boot.loader.efi.canTouchEfiVariables = true;
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
 
-    kernelModules = [ "v4l2loopback" ];
-    extraModulePackages = [
-      config.boot.kernelPackages.v4l2loopback
-    ];
-    extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 video_nr=9
-    '';
+    #kernelModules = [ "v4l2loopback" ];
+    #extraModulePackages = [
+    #config.boot.kernelPackages.v4l2loopback
+    #];
+    #extraModprobeConfig = ''
+    #options v4l2loopback exclusive_caps=1 video_nr=9
+    #'';
   };
 }
