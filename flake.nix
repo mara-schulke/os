aec909f124108c8229841d0a3f73e3f4847b8522
Mara Schulke <mail@maras.cloud>
2025-07-31 18:26:51 +0200

    Unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mara's Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
