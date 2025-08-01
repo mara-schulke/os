ed445afd7bedea1835ea05ceb6ebc691a35f9b7e
Mara Schulke <mail@maras.cloud>
2025-08-01 10:07:55 +0200

    Add spectre

--- modules/home/editor/nvim/editing/search.nix
+++ modules/home/editor/nvim/editing/search.nix
@@ -16,6 +16,8 @@ in
   programs.nixvim = {
     extraPlugins = [ visual-star-search ];
 
+    plugins.spectre.enable = true;
+
     keymaps = [
       # No Highlighting
       {
@@ -38,9 +40,18 @@ in
       }
       {
         key = "<leader>rc";
-        action = "s///gc<Left><Left><Left>";
+        action = ":s///gc<Left><Left><Left>";
         options.desc = "Replace All (Confirm)";
       }
+      # Spectre
+      {
+        key = "<leader>/";
+        action = "<cmd>lua require(\"spectre\").toggle()<CR>";
+        options.desc = "Search";
+      }
+      #vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', { desc = "Search current word" })
+      #vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
+      #vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Search on current file" })
     ];
   };
 }
