160cd6db8f814c22a876b97871681ff596b1c26b
Mara Schulke <mail@maras.cloud>
2025-08-03 15:10:50 +0200

    Search bindings

--- modules/home/editor/nvim/editing/search.nix
+++ modules/home/editor/nvim/editing/search.nix
@@ -45,13 +45,10 @@ in
       }
       # Spectre
       {
-        key = "<leader>/";
+        key = "<A-/>";
         action = "<cmd>lua require(\"spectre\").toggle()<CR>";
         options.desc = "Search";
       }
-      #vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', { desc = "Search current word" })
-      #vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
-      #vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', { desc = "Search on current file" })
     ];
   };
 }
