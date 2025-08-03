f12bf37ee8ef95b349f3d274506f85650f73e5f9
Mara Schulke <mail@maras.cloud>
2025-08-03 15:23:04 +0200

    Install mold

--- modules/home/programs/utilities.nix
+++ modules/home/programs/utilities.nix
@@ -17,9 +17,22 @@
     tree
     curl
 
+    mold
+    lld
+
     python312
     python312Packages.huggingface-hub
 
     lsof
   ];
+
+  home.file.".cargo/config.toml".text = ''
+    [target.aarch64-apple-darwin]
+    linker = "clang"
+    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+
+    [target.x86_64-unknown-linux-gnu]
+    linker = "clang"
+    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
+  '';
 }
