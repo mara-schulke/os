98d5c46c469d2b8b8557b2638f2f8b1b5ba036a3
Mara Schulke <mail@maras.cloud>
2025-08-03 13:58:26 +0200

    Terminal integration

--- modules/home/editor/nvim/terminal/default.nix
+++ modules/home/editor/nvim/terminal/default.nix
@@ -18,6 +18,16 @@ in
 
     keymaps = [
       {
+        key = "<C-S-r>";
+        action = "<cmd>luafile $MYVIMRC<cr>";
+      }
+      {
+        key = "<A-f>";
+        action = "<cmd>ToggleTerm direction=float name=scratchpad<CR>";
+        options.desc = "Open scratchpad";
+        options.silent = true;
+      }
+      {
         key = "<A-,>";
         action = "<cmd>ToggleTerm size=15 direction=horizontal name=terminal<CR>";
         options.desc = "Open terminal";
@@ -25,7 +35,7 @@ in
       }
       {
         key = "<A-.>";
-        action = "<cmd>ToggleTerm size=75 direction=vertical<CR>";
+        action = "<cmd>ToggleTerm size=75 direction=vertical name=terminal<CR>";
         options.desc = "Open terminal";
         options.silent = true;
       }
@@ -34,22 +44,26 @@ in
     extraConfigLuaPost = ''
       require("toggleterm").setup()
 
-      function _G.set_terminal_keymaps()
-        local opts = {buffer = 0}
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      function _G.term_bind_keys()
+        local opts = { buffer = 0 }
+        vim.cmd([[ startinsert! ]], opts)
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>:]], opts)
         vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<A-f>', [[<Cmd>ToggleTerm direction=float<CR>]], opts)
         vim.keymap.set('t', '<A-,>', [[<Cmd>ToggleTerm direction=horizontal<CR>]], opts)
         vim.keymap.set('t', '<A-.>', [[<Cmd>ToggleTerm direction=vertical<CR>]], opts)
         vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<C-l><Cmd>wincmd l<CR>]], opts)
       end
 
-      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-      vim.cmd('autocmd! TermOpen term://*toggleterm lua set_terminal_keymaps()')
+      vim.cmd('autocmd! TermOpen term://* lua term_bind_keys()')
+      vim.cmd('autocmd! TermOpen term://*toggleterm lua term_bind_keys()')
+      vim.cmd('autocmd! BufEnter term://* startinsert!')
+      vim.cmd('autocmd! BufEnter term://*toggleterm startinsert!')
+      vim.cmd('autocmd! BufWinEnter term://* startinsert!')
+      vim.cmd('autocmd! BufWinEnter term://*toggleterm startinsert!')
     '';
   };
 }
