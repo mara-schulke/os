7216b14012d6264f27afa3017e5aa1a2b3753af3
Mara Schulke <mail@maras.cloud>
2025-08-04 12:51:13 +0200

    Disable mold

--- modules/home/pkgs/utilities.nix
+++ modules/home/pkgs/utilities.nix
@@ -17,7 +17,6 @@
     tree
     curl
 
-    mold
     lld
 
     python312
@@ -25,14 +24,4 @@
 
     lsof
   ];
-
-  home.file.".cargo/config.toml".text = ''
-    [target.aarch64-apple-darwin]
-    linker = "clang"
-    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
-
-    [target.x86_64-unknown-linux-gnu]
-    linker = "clang"
-    rustflags = ["-C", "link-arg=-fuse-ld=mold"]
-  '';
 }
