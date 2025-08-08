2b078932da2fa3b8045083265837faaf869e0b79
Mara Schulke <mail@maras.cloud>
2025-08-09 00:39:15 +0200

    Diagnostics setup

--- modules/home/editor/nvim/lsp/diagnostics.nix
+++ modules/home/editor/nvim/lsp/diagnostics.nix
@@ -1,8 +1,58 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  workspace-diagnostics = pkgs.vimUtils.buildVimPlugin {
+    pname = "workspace-diagnostics.nvim";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "artemave";
+      repo = "workspace-diagnostics.nvim";
+      rev = "main";
+      sha256 = "sha256-jSpKaKnGyip/nzqU52ypWLgoCtvccYN+qb5jzlwAnd4=";
+    };
+  };
+in
 {
   programs.nixvim = {
+    extraPlugins = [ workspace-diagnostics ];
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<A-d>";
+        action = ''<cmd>Trouble diagnostics toggle<CR>'';
+        options.desc = "Open diagnostics";
+      }
+      {
+        mode = "n";
+        key = "<leader>d";
+        action = ''<cmd>Scan<CR>'';
+        options.desc = "Scan workspace diagnostics";
+      }
+      {
+        mode = "n";
+        key = "<A-t>";
+        action = ''
+          function()
+            vim.api.nvim_feedkeys(":TodoTrouble ", "n", false)
+          end
+        '';
+        lua = true;
+        options = {
+          desc = "Git ...";
+        };
+      }
+    ];
+
     extraConfigLua = ''
+      vim.api.nvim_create_user_command('Scan', function (args)
+        for _, client in ipairs(vim.lsp.buf_get_clients()) do
+          require("workspace-diagnostics").populate_workspace_diagnostics(client, 0)
+        end
+
+        print("Scanning workspace.. 💗")
+      end, { desc = "Scan workspace for diagnostics" })
+
       vim.fn.sign_define("DiagnosticSignError", {
         texthl = "DiagnosticSignError",
         text = "E",
