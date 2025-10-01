de7a5159563b91536a4030f08d3ec1166c9e0b18
Mara Schulke <mail@maras.cloud>
2025-10-01 10:27:27 +0200

    Build my config with cabal

--- modules/system/wm/default.nix
+++ modules/system/wm/default.nix
@@ -1,15 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  xmonadPkg = pkgs.haskellPackages.callCabal2nix "xmonad-config" ./. {};
+in
 {
   services.xserver.windowManager.xmonad = {
     enable = true;
     enableContribAndExtras = true;
     enableConfiguredRecompile = true;
-    config = builtins.readFile "${./.}/xmonad.hs";
+    config = builtins.readFile ./xmonad.hs;
     extraPackages = hpkgs: [
       hpkgs.xmonad
       hpkgs.xmonad-extras
       hpkgs.xmonad-contrib
+      xmonadPkg
     ];
   };
 }
