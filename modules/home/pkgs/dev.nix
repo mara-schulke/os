fac51f5af8333c46a02290c8ab537aa0fd85437b
Mara Schulke <mail@maras.cloud>
2025-11-26 12:33:59 +0100

    Git settings

--- modules/home/pkgs/dev.nix
+++ modules/home/pkgs/dev.nix
@@ -21,23 +21,26 @@ in
 
   programs.git = {
     enable = true;
-    aliases = {
-      lg = "log --graph --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%I:%S' --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white) - %C(dim white)%ar%C(reset) - %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-      st = "status -s";
-      co = "checkout";
-      fa = "fetch --all --prune";
-      pa = "pull --all";
-      ba = "branch -v --all";
-      info = "!onefetch --no-art";
-    };
-
-    userName = "Mara Schulke";
-    userEmail = "mail@maras.cloud";
-
     lfs.enable = true;
     signing.signByDefault = true;
 
-    extraConfig = {
+    settings = {
+      alias = {
+        lg = "log --graph --abbrev-commit --decorate --date=format:'%Y-%m-%d %H:%I:%S' --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white) - %C(dim white)%ar%C(reset) - %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        st = "status -s";
+        co = "checkout";
+        fa = "fetch --all --prune";
+        pa = "pull --all";
+        ba = "branch -v --all";
+        info = "!onefetch --no-art";
+      };
+
+      user = {
+        name = "Mara Schulke";
+        email = "mail@maras.cloud";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQzqzXz57EF6Z9anpzFhK4a1LscLC+e4W4IWiuJ0d5G";
+      };
+
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
 
@@ -51,7 +54,6 @@ in
       "gpg \"ssh\"".program = opSign;
 
       commit.gpgsign = true;
-      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQzqzXz57EF6Z9anpzFhK4a1LscLC+e4W4IWiuJ0d5G";
     };
   };
 }
