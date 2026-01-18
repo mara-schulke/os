25a89d13f302664aaef1286f7afa66480183f2dd
Mara Schulke <mail@maras.cloud>
2026-01-18 22:49:19 +0100

    Add goyo

--- modules/home/editor/nvim/editing/writing.nix
+++ modules/home/editor/nvim/editing/writing.nix
@@ -1,7 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  goyo = pkgs.vimUtils.buildVimPlugin {
+    pname = "goyo-vim";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "junegunn";
+      repo = "goyo.vim";
+      rev = "master";
+      sha256 = "sha256-pDt7T1U1bqKveAq0CBWTtK2mdOzf8IbfmCi1fcpB2c8=";
+    };
+  };
+in
 {
+
   programs.nixvim = {
+    extraPlugins = [ goyo ];
+
     plugins.vimtex = {
       enable = true;
       texlivePackage = null; # Use system texlive
