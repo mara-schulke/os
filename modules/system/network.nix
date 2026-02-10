8774760cca0eb12d381f00d632b734a836fe2de0
Mara Schulke <mail@maras.cloud>
2026-02-10 22:37:34 +0100

    Sunshine

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,19 +1,53 @@
 { lib, ... }:
 
 {
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
   networking.wireless.enable = lib.mkForce false;
   networking.networkmanager.enable = true;
   programs.nm-applet.enable = true;
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [
-      22
-      80
-      443
-      3000
-      8000
-      8080
-    ];
+    allowedTCPPorts =
+      let
+        general = [
+          22
+          80
+          443
+          3000
+          8000
+          8080
+        ];
+
+        sunshine = [
+          47984
+          47989
+          47990
+          48010
+        ];
+      in
+      general ++ sunshine;
+
+    allowedUDPPortRanges =
+      let
+        sunshine = [
+          {
+            from = 47998;
+            to = 48000;
+          }
+          {
+            from = 8000;
+            to = 8010;
+          }
+        ];
+      in
+      sunshine;
   };
+
 }
